3fb6cfb13a305a4109bbb1d42d3ac50a7662a848
Dean Radcliffe <deanius@users.noreply.github.com>
2020-04-30 11:32:11 -0500

    Trigger a 'tick' event that increments state and logs.
    Note how the browser can never know when to refresh.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-var http = require("http");
+const { trigger, listen: on } = require("polyrhythm");
 
-//create a server object:
+const initialState = { count: 0 };
+const state = initialState;
+
+on("tick", () => {
+  state.count += 1;
+  console.log(`Ticked ${state.count} times so far! 🤯`);
+});
+
+const intervalId = setInterval(() => {
+  trigger("tick");
+  state.count >= 50 && clearInterval(intervalId);
+}, 2000);
+// Run an endpoint
+var http = require("http");
 http
   .createServer(function(req, res) {
-    res.write("Hello World!"); //write a response to the client
+    res.write(`The count is currently ${state.count}`); //write a response to the client
     res.end(); //end the response
   })
   .listen(8080); //the server object listens on port 8080
